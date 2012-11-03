9189d364d3fc53892b92d168effe33c802ad3f56
zzfnohell <zzfnohell@gmail.com>
2012-11-03 17:30:35 +0800

    finish load test

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ src_path=`dirname $src_path`
 file=`readlink -f $1`
 echo "Loading file..."
 echo $file
-maxima --init-mac="$src_path/init.mac"
+maxima --init-mac="$src_path/init.mac" -b="$file"
 echo "Loading file done."
