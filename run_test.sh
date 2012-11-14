8b43b761c6c4a4822883f7abef3fbff0ba00800f
zzfnohell <zzfnohell@gmail.com>
2012-11-14 22:12:53 +0800

    add usage to run_test.sh file

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+usage()
+{
+    echo "Use run_test.sh like:"
+    echo "run_test.sh {test_file.mac}"
+};
+
+if [ $# -ne 1 -o ! -f $1 ]; then
+usage;
+exit;
+fi;
+
 src_path=`readlink -f $0`
 src_path=`dirname $src_path`
 file=`readlink -f $1`
@@ -6,3 +18,4 @@ echo "Loading test..."
 echo $file
 maxima --init-mac="$src_path/init.mac" -b="$file"
 echo "Loading test done."
+
